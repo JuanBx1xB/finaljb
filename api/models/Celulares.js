10d3b86848fdb13a153b80209dafd624bfb8a6aa
JuanBx1xB <juanbonilla0102@gmail.com>
2023-06-04 11:06:45 -0500

    api

---
+++ api/models/Celulares.js
@@ -0,0 +1,18 @@
+/**
+ * Celulares.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+  description: {type: 'string', },
+  stock: {type: 'string', },
+
+  },
+
+};
+
